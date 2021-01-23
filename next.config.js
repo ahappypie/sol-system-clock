67425774f0b7434b65d0d786ce390ac06822e9c9
Brian Bagdasarian <bbagdasarian.scs@gmail.com>
2021-01-22 23:07:12 -0800

    Check the url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const repo_url = 'sol-system-clock'
 
-module.exports = {
-    basePath: `/${repo_url}`,
-    assetPrefix: `/${repo_url}/`
+if(!process.env.DEV) {
+    module.exports = {
+        basePath: `/${repo_url}`,
+        assetPrefix: `/${repo_url}/`
+    }
 }
